2f3c1aea2965177746015e4bf0d378906b801f94
くろけまり <kurokemari@kurokemarinoMacBook-Pro.local>
2020-05-28 15:01:45 +0900

    メイン画面作成

---
+++ PHP/main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>しりとりアプリ</title>
+</head>
+<body>
+    <!-- タイトル表示 -->
+    <div>
+     <p>しりとりアプリ</p>
+    </div>
+
+    <!-- 挑戦者欄 -->
+    <form action="" class="challenger">
+     <input type="text" name="word" id="word">
+     <input type="submit" value="送信">
+    
+    
+    </form>
+
+
+
+
+
+
+
+</body>
+</html>
