3f78f8e7f26ab837da69ceb0631f2489295021cf
くろけまり <kurokemari@kurokemarinoMacBook-Pro.local>
2020-05-29 19:49:30 +0900

    更新されるかチェック

--- PHP/Shiritori.php
+++ PHP/Shiritori.php
@@ -12,17 +12,16 @@
      <p>つべこべ言わずに尻を取れ！！！</p>
     </div>
     <div class="subtitel">
-     <p>えんまちゃんが性格の悪さをはっきする地獄のしりとりだよ！</p>
+     <p>えんまちゃんが性格の悪さをはっきする地獄のしりとりだよ！！</p>
     </div>
     </header>
     <!-- 挑戦者欄 -->
     <form action="post" class="challenger">
      <input type="text" name="text" id="text">
      <input type="submit" value="送信">
-    
-    
     </form>
-
+    <!-- 履歴表示 -->
+    <div class="review"></div>
 
 
 
