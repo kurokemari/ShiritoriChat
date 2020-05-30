20e905c5a7bb623ed599fec800d39dc1b2b47146
くろけまり <kurokemari@kurokemarinoMacBook-Pro.local>
2020-05-30 11:59:08 +0900

    最後の文字アップデート

--- PHP/insert.php
+++ PHP/insert.php
@@ -1,6 +1,8 @@
 <?php
 
 $text = $_POST["text"];
+$last = "";
+$FLG = 0;
 
 //2. DB接続します
 //ここから作成したDBに接続をしてデータを登録します
@@ -10,15 +12,19 @@ try {
     exit('DbConnectError:'.$e->getMessage());
   }
   
-  
-  //３．データ登録SQL作成 //ここにカラム名を入力する
-  //テーブル名を入力します
-  $last = $pdo->prepare("RIGHT($text,1)");
-  $stmt = $pdo->prepare("INSERT INTO ShiritoriChat_text_table(id, text, last)VALUES(NULL, :text, :last");
+  //３．データ登録SQL作成
+  // 最後の文字は空欄を代入
+  $stmt = $pdo->prepare("INSERT INTO ShiritoriChat_text_table (id, text, last, FLG) VALUES (NULL, :text, :last, :FLG)");
   $stmt->bindValue(':text', $text, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
   $stmt->bindValue(':last', $last, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+  $stmt->bindValue(':FLG', $FLG, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
   $status = $stmt->execute();
   
+  // 最後の文字をUPDATEする
+  $stmt0 = $pdo->prepare("UPDATE ShiritoriChat_text_table SET last=RIGHT(:text,1) WHERE text=:text");
+  $stmt0->bindValue(':text', $text, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+  $status0 = $stmt0->execute();
+
   //４．データ登録処理後
   if($status==false){
     //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
